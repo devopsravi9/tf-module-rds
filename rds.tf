d5d61e00fa55f49418c66655033bc6277fd3bbcf
Ravi <ravi@gmail.com>
2022-08-01 08:07:25 +0530

    ec2 code

--- rds.tf
+++ rds.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "default" {
   engine_version       = var.RDS_ENGINE_VERSION
   instance_class       = var.RDS_INSTANCE_CLASS
   username             = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]
-  password             = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_pass"]
+  password             = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_PASS"]
   parameter_group_name = aws_db_parameter_group.main.name
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.main.name
