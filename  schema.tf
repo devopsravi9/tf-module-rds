fbfaad02e636d9837d907a533d471958ae2de827
Ravi <ravi@gmail.com>
2022-08-08 10:44:51 +0530

    ec2 code

---  schema.tf
+++  schema.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "schema" {
   provisioner "local-exec" {
     command = <<EOF
-curl -s -L -o /etc/yum.repos.d/mysql.repo "https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
 cd /tmp
 unzip -o mysql.zip
 cd mysql-main
